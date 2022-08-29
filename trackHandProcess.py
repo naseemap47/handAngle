15e42d8b984ba17bdeaa9a06a40ccbbfc670aa2e
NASEEM A P <naseemalassampattil@gmail.com>
2022-08-29 11:58:17 +0530

    added cam port, cam width and height

--- trackHandProcess.py
+++ trackHandProcess.py
@@ -1,17 +1,35 @@
 import cv2
 import mediapipe as mp
 import os
+import argparse
 
 
-cap = cv2.VideoCapture(0)
+ap = argparse.ArgumentParser()
+ap.add_argument("--cam", type=int, required=True,
+                help="Web-cam port")
+ap.add_argument("--cam_width", type=int, required=True,
+                help="Size of web-cam width")
+ap.add_argument("--cam_height", type=int, required=True,
+                help="Size of web-cam height")
+
+
+args = vars(ap.parse_args())
+cam = args["cam"]
+cam_width = args["cam_width"]
+cam_height = args["cam_height"]
+
+cap = cv2.VideoCapture(cam)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
+
 mp_hand = mp.solutions.hands
 hand = mp_hand.Hands(max_num_hands=1)
 mp_draw = mp.solutions.drawing_utils
 
 # Ref-points
-ref1 = (250, 400)
-ref2 = (20, 180)
-ref3 = (250, 180)
+ref1 = (int(cam_width*0.46875), int(cam_height*0.8333))
+ref2 = (int(cam_width*0.109375), int(cam_height*0.375))
+ref3 = (int(cam_width*0.46875), int(cam_height*0.375))
 
 while True:
     success, img = cap.read()
@@ -48,8 +66,8 @@ while True:
 
     # Process Ending Area
     cv2.rectangle(
-        img, (ref2[0]-20, ref2[1]-20),
-        (ref2[0]+20, ref2[1]+20), (255, 255, 0), 3
+        img, (ref2[0]-30, ref2[1]-30),
+        (ref2[0]+30, ref2[1]+30), (255, 255, 0), 3
     )
 
     # Flip Webcam feed
@@ -81,8 +99,8 @@ while True:
                     cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3
                 )
 
-            if (ref2[0]-20) < line_points[0][0] < (ref2[0]+20):
-                if (ref2[1]-20) < line_points[0][1] < (ref2[1]+20):
+            if (ref2[0]-30) < line_points[0][0] < (ref2[0]+30):
+                if (ref2[1]-30) < line_points[0][1] < (ref2[1]+30):
                     cv2.putText(
                         img, 'Work Ended!!', (40, 50),
                         cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3
